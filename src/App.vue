78fa051ea331ebc2a4648f36556bdbb5ce5ee569
Kingsley <firebox@ikingsley.com>
2023-08-05 15:10:58 +0200

    Alpha Trade init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Header from "@/components/Header.vue";
+import "@/index.css";
+</script>
+<template>
+  <Header />
+</template>
+<style>
+#app {
+  font-family: var(--font-regular);
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.main {
+  max-width: var(--max-width);
+  margin: 0 auto;
+}
+
+.heading1 {
+  font-family: var(--font-bold);
+}
+
+.component-list {
+  padding: 0 0 0 12px;
+}
+</style>
